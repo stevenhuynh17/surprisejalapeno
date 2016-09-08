d305a832f24643a7ced038866d0c6dc2a1ed73c6
D. Wren <drawnwren@gmail.com>
2016-09-08 12:03:03 -0700

    Added some documentation.

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,5 +2,6 @@ const bing = require('../api_controllers/bing');
 
 module.exports = (app) => {
   app.get('/', (req, res) => res.send('Hello world!'));
-  app.get('/query', bing.searchHandler);
+  app.get('/query', bing.searchHandler); // expects the URI to have a query parameter
+  // i.e. /query?q='dogs'
 };
