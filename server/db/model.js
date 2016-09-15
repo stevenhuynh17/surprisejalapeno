c4761b60d0afebe51150af520976d1fda9052bf2
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-15 16:29:34 -0700

    Implement sequelize to write and fetch with db and watson

--- server/db/model.js
+++ server/db/model.js
@@ -4,15 +4,15 @@ module.exports = {
     // all methods return a promise
     // getters resolve with -> [{...}, {...}, ...]
   news: {
-    fetchAll() {
-      return db.select().from('news')
-      .catch(err =>
-          console.log(`Error fetching data from "news" table ${err}`));
-    },
-    getByTitle(title) {
-      return db('news').where('title', title)
-      .catch(err => console.log(`Error getting record by title ${err}`));
-    },
+    // fetchAll() {
+    //   return db.select().from('news')
+    //   .catch(err =>
+    //       console.log(`Error fetching data from "news" table ${err}`));
+    // },
+    // getByTitle(title) {
+    //   return db('news').where('title', title)
+    //   .catch(err => console.log(`Error getting record by title ${err}`));
+    // },
     getByLocation(loc) {
       // expect loc to be formatted as {lat, lng, rad} where rad = radius in
       // miles to search within using Haversine Formula to calculate distances
@@ -20,30 +20,43 @@ module.exports = {
       // "distance" property that represents the entity's distance from the
       // midpoint
       // console.log('Input values to getByLocation in model.js: ', loc);
-      return db
+      // return db
       // .select(db.raw(`*, (
       //   3959 * acos(cos(radians(${loc.lat})) * cos(radians(lat)) *
       //   cos(radians(lng) - radians(${loc.lng})) + sin(radians(${loc.lat})) *
       //   sin(radians(lat)))
       //   ) as distance`
-      // ))
-      .select(db.raw('*'))
-      .from('news')
-      // .having('distance', '<', loc.rad)
-      // .orderBy('distance', 'asc')
-      // .limit(100)
-      .catch(err => console.log('Error getting by location'));
+      // // ))
+      // .select(db.raw('*'))
+      // .from('news')
+      // // .having('distance', '<', loc.rad)
+      // // .orderBy('distance', 'asc')
+      // // .limit(100)
+      // .catch(err => console.log('Error getting by location'));
+      return db.findAll()
+        .then((data) => {
+          console.log('Results of query in getByLocation: ', data);
+        })
+        .catch((err) => {
+          console.log('Error fetching all data: ', err);
+        });
     },
     add(data) {
       // expects data to be formatted as
       // {title: '', rating: num, category: '', ...etc}
       // can take an array of data objects -> [{...}, {...}, ...]
       // resolves promise with id of first inserted record -> [id]
-      return db('news').insert(data, 'id')
-      .catch(err => console.log(`Error inserting into "news" table`
-        // `${err}`
-        ));
-
+      // return db('news').insert(data, 'id')
+      // .catch(err => console.log(`Error inserting into "news" table`
+      //   // `${err}`
+      //   ));
+      return db.bulkCreate(data, { ignoresDuplicates: true })
+        .then((dbRes) => {
+          console.log('Data returned from bulkCreate: ', dbRes);
+        })
+        .catch((err) => {
+          console.log('Error with bulkCreate: ', err);
+        });
       // Knex does not have upsert. Above 'add' breaks on duplicate articles
       // use db.raw(<YOUR QUERY HERE>) to build your own upsert
       // return db.schema.raw(``)
