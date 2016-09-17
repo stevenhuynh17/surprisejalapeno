3f5370572d004b3590855d20b5f0c849fb404ea1
Steven <stevenhuynh17@gmail.com>
2016-09-16 20:31:43 -0700

    Adding config file

---
+++ env/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  watsonAPI: 'Your API here',
+  googGeoAPI: 'Your API here',
+  DB_HOST: 'Your Credentials here',
+  DB_USER: 'Your Credentials here',
+  DB_PASSWORD: 'Your Credentials here'
+};
